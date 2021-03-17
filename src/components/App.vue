00ffbf55776cf1e1c4581fea79308dd634d54d28
Laurent Bernabe <laurent.bernabe@gmail.com>
2021-03-17 10:10:16 +0100

    Using Vue Composition API

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,32 +1,32 @@
 <template>
-    <Page>
-        <ActionBar title="Welcome to NativeScript-Vue!"/>
-        <GridLayout columns="*" rows="*">
-            <Label class="message" :text="msg" col="0" row="0"/>
-        </GridLayout>
-    </Page>
+  <Page>
+    <ActionBar title="Welcome to NativeScript-Vue!" />
+    <GridLayout columns="*" rows="*">
+      <Label class="message" :text="msg" col="0" row="0" />
+    </GridLayout>
+  </Page>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        msg: 'Hello World!'
-      }
-    }
+import { ref } from "@vue/composition-api";
+export default {
+  setup() {
+    const msg = "Dropbox auth";
+    return {msg};
   }
+};
 </script>
 
 <style scoped>
-    ActionBar {
-        background-color: #53ba82;
-        color: #ffffff;
-    }
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
 
-    .message {
-        vertical-align: center;
-        text-align: center;
-        font-size: 20;
-        color: #333333;
-    }
+.message {
+  vertical-align: center;
+  text-align: center;
+  font-size: 20;
+  color: #333333;
+}
 </style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from "nativescript-vue";
+import App from "./components/App.vue";
+import VueDevtools from "nativescript-vue-devtools";
+import VueCompositionAPI from "@vue/composition-api";
+
+if (TNS_ENV !== "production") {
+  Vue.use(VueDevtools);
+}
+import store from "./store";
+
+Vue.use(VueCompositionAPI);
+
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = TNS_ENV === "production";
+
+new Vue({
+  store,
+  render: (h) => h("frame", [h(App)]),
+}).$start();
